855253fd7e7ef0b731a2807edfeb84432fba9570
lijiarui <1063153785@qq.com>
2017-10-27 10:42:43 +0800

    master main

---
+++ demo-core/src/main/java/cn/ligiarui/core/master.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2001-2017 GuaHao.com Corporation Limited. All rights reserved. 
+ * This software is the confidential and proprietary information of GuaHao Company. 
+ * ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only 
+ * in accordance with the terms of the license agreement you entered into with GuaHao.com.
+ */
+package cn.ligiarui.core;
+
+/**
+ * TODO
+ *
+ * @author lijiarui
+ * @version V1.0
+ * @since 2017-10-27 10:40
+ */
+public class master {
+
+    public static void main(String[] args){
+        System.out.println("master");
+    }
+}
